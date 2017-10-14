c43921991a6855d68d060ea212f610afbf38dc47
Ariel Herman <aeh.herman@gmail.com>
2017-10-14 11:40:38 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: 'Ariel', last_name: 'Herman')
+Student.create(first_name: 'Mr', last_name: 'Cool')
